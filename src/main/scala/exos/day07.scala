990a723b35944b6d62dd1d58e28d78edc10aabf9
Pragmatias <pragmatias@gmail.com>
2024-02-12 01:07:45 +0100

    End of Day7 and Data for Day8

--- src/main/scala/exos/day07.scala
+++ src/main/scala/exos/day07.scala
@@ -1,13 +1,10 @@
 package exos
 
 import fr.pragmatias.tools.Utils
-import scala.runtime.stdLibPatches.language.`3.3`
-import scala.runtime.stdLibPatches.language.`3.2`
 
 class Day07() :
 
   val dataFileS1 = "day07_step1.txt"
-  val dataFileS2 = "day07_step2.txt"
   val dataFileFull = "day07.txt"
 
   enum HandType : 
@@ -18,9 +15,14 @@ class Day07() :
       val test = h.isBetterThan(i.h)
       // println("Compare %s - %s = %s".format(h,i.h,test))
       return test
+    def isBetterThanStep2(i:Input) : Boolean =
+      val test = h.isBetterThanStep2(i.h)
+      // println("Compare %s - %s = %s".format(h,i.h,test))
+      return test
 
   class Hand(val hand:String) :
     val typeH : HandType = getHandType()
+    val typeJ : HandType = replaceJoker()
     private def getHandType() : HandType = 
       val tmp = hand.groupBy(identity).map(x => (x._1,x._2.length)).toList 
       return tmp.length match 
@@ -48,6 +50,15 @@ class Day07() :
               case "K" => 13
               case "Q" => 12
               case "J" => 11
+              case "T" => 10
+              case _ => (card+"").toInt
+    
+    def getValueStep2(card:Char) : Int = 
+      return card+"" match
+              case "A" => 14
+              case "K" => 13
+              case "Q" => 12
+              case "J" => 1
               case "T" => 10
               case _ => (card+"").toInt
    
@@ -83,8 +94,38 @@ class Day07() :
           getValue(hand(4)) > getValue(h.hand(4)) 
         }
       }
+    def isBetterThanStep2(h:Hand) : Boolean =
+      if (getValue(typeJ) != getValue(h.typeJ)) {
+        // compare type
+        getValue(typeJ) > getValue(h.typeJ)
+        
+      } else {
+        // compare char
+        if (getValueStep2(hand(0)) != getValueStep2(h.hand(0))) {
+          getValueStep2(hand(0)) > getValueStep2(h.hand(0)) 
+        } else
+        if (getValueStep2(hand(1)) != getValueStep2(h.hand(1))) {
+          getValueStep2(hand(1)) > getValueStep2(h.hand(1)) 
+        } else
+        if (getValueStep2(hand(2)) != getValueStep2(h.hand(2))) {
+          getValueStep2(hand(2)) > getValueStep2(h.hand(2)) 
+        } else
+        if (getValueStep2(hand(3)) != getValueStep2(h.hand(3))) {
+          getValueStep2(hand(3)) > getValueStep2(h.hand(3)) 
+        } else {
+          getValueStep2(hand(4)) > getValueStep2(h.hand(4)) 
+        }
+      }
     override def toString() : String = 
       "Hand(%s)".format(hand)
+
+    def replaceJoker() : HandType =
+      val listCard = List("A","K","Q","T","9","8","7","6","5","4","3","2")
+      if (hand.contains("J")) {
+        listCard.map(x => hand.replaceAll("J",x)).map(Hand(_)).sortWith(_.isBetterThan(_)).head.typeH
+      } else {
+        typeH
+      }
 
 
   def calcResult(hands:IndexedSeq[Input],result:Long) : Long =
@@ -105,18 +146,26 @@ class Day07() :
     }
     return calcResult(sortedHands,0)
 
-  def runStep2(p: os.Path, f: String, debug:Boolean) : Int = 
-    return 0
+  def runStep2(p: os.Path, f: String, debug:Boolean) : Long = 
+    val data : IndexedSeq[String] = os.read.lines(p / f)
+    val hands : IndexedSeq[Input] = data.map(x => x.split(" ")).map(x => Input(Hand(x(0)),x(1).toInt))
+    val sortedHands : IndexedSeq[Input] = hands.sortWith(_.isBetterThanStep2(_))
+    if (debug) {
+      println(data)
+      println(hands.head)
+      println(sortedHands)
+    }
+    return calcResult(sortedHands,0)
 
 
 @main def runDay07() = 
   val day = Day07()
   // println("Step1 : Sample")
   // println(day.runStep1(Utils.dataSamplePath,day.dataFileS1,true))
-  println("Step1 : Full")
-  println(day.runStep1(Utils.dataFullPath,day.dataFileFull,true))
-  // println("Step2 : Sample")
-  // println(day.runStep2(Utils.dataSamplePath,day.dataFileS1,true))
-  // println("Step2 : Full")
-  // println(day.runStep2(Utils.dataFullPath,day.dataFileFull,false))
+  // println("Step1 : Full")
+  // println(day.runStep1(Utils.dataFullPath,day.dataFileFull,true))
+  println("Step2 : Sample")
+  println(day.runStep2(Utils.dataSamplePath,day.dataFileS1,true))
+  println("Step2 : Full")
+  println(day.runStep2(Utils.dataFullPath,day.dataFileFull,true))
   
--- src/main/scala/exos/dayXX.scala
+++ src/main/scala/exos/dayXX.scala
@@ -5,21 +5,22 @@ import fr.pragmatias.tools.Utils
 class DayXX() :
 
   val dataFileS1 = "dayXX_step1.txt"
-  val dataFileS2 = "dayXX_step2.txt"
   val dataFileFull = "dayXX.txt"
 
 
   def runStep1(p: os.Path, f: String, debug:Boolean) : Int =
+    val data : geny.Generator[String] = os.read.lines.stream(p / f)
     return 0
 
   def runStep2(p: os.Path, f: String, debug:Boolean) : Int = 
+    val data : geny.Generator[String] = os.read.lines.stream(p / f)
     return 0
 
 
 @main def runDayXX() = 
   val day = DayXX()
-  // println("Step1 : Sample")
-  // println(day.runStep1(Utils.dataSamplePath,day.dataFileS1,true))
+  println("Step1 : Sample")
+  println(day.runStep1(Utils.dataSamplePath,day.dataFileS1,true))
   // println("Step1 : Full")
   // println(day.runStep1(Utils.dataFullPath,day.dataFileFull,false))
   // println("Step2 : Sample")
