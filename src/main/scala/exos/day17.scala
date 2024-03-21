5c3a917ccd8be26d61572ae07d7d3d3ee1823843
Pragmatias <pragmatias@gmail.com>
2024-03-21 15:44:20 +0100

    Maj Day17 to have better perf (using priorityqueue with ordering by default)

--- src/main/scala/exos/day17.scala
+++ src/main/scala/exos/day17.scala
@@ -3,6 +3,7 @@ package fr.pragmatias.exos
 import fr.pragmatias.tools.Utils
 import scala.annotation.tailrec
 import scala.collection.mutable.Map
+import scala.collection.mutable.PriorityQueue
 
 class Day17() :
 
@@ -11,11 +12,7 @@ class Day17() :
 
   enum Dir :
     case U,D,L,R
-    def goStraight = this match
-      case Dir.U => Dir.U
-      case Dir.D => Dir.D
-      case Dir.L => Dir.L
-      case Dir.R => Dir.R
+    def goStraight = this
     def goLeft = this match
       case Dir.U => Dir.L
       case Dir.D => Dir.R
@@ -28,8 +25,7 @@ class Day17() :
       case Dir.R => Dir.D
      
     
-  class HeatMap(data : List[String]) :
-    val map : Array[Array[Int]] = data.map(_.toArray.map(_.toString.toInt)).toArray
+  class HeatMap(map : Array[Array[Int]]) :
     val limX = map.head.length
     val limY = map.length
     override def toString() : String = map.map(l => l.mkString("-")).mkString("\n")
@@ -37,13 +33,15 @@ class Day17() :
     def isInHM(p:Pos) : Boolean =
       return p.x >= 0 && p.y >= 0 && p.x < limX && p.y < limY
 
+  def loadHeatMap(data:List[String]) : Array[Array[Int]] = 
+    return data.map(_.toArray.map(_.asDigit)).toArray
 
   case class Pos(x:Int,y:Int) :
     def move(d:Dir) : Pos = d match
-      case Dir.U => Pos(x,y-1)
-      case Dir.D => Pos(x,y+1)
-      case Dir.L => Pos(x-1,y)
-      case Dir.R => Pos(x+1,y)
+      case Dir.U => copy(y=y-1)
+      case Dir.D => copy(y=y+1)
+      case Dir.L => copy(x=x-1)
+      case Dir.R => copy(x=x+1)
     
 
   case class State(p : Pos, d : Dir, rep : Int) :
@@ -69,46 +67,50 @@ class Day17() :
     val startState = State(Pos(0,0),Dir.R,0)
     minHeat(startState) = 0
 
+    val stateQueue : PriorityQueue[State] = PriorityQueue.empty[State](Ordering.by(minHeat)).reverse
+
+
     @tailrec
-    def findPath(ls : List[State]) : Long =
+    def findPath(ls : PriorityQueue[State]) : Long =
       if (ls.isEmpty) then 
          return 0L
       else
-        val lss = ls.sortBy(minHeat)
-        val s = lss.head
+        val s = ls.dequeue()
         if (s.p == pFinal) then 
           return minHeat(s)
         else 
           val ns = s.nextStates(hm,minRep,maxRep).filterNot(minHeat.contains)
           ns.foreach(x => {
             minHeat(x) = minHeat(s)+hm.getHeat(x.p)
+            ls.enqueue(x)
           })
-          return findPath(ns:::lss.tail)
+          return findPath(ls)
 
-    return findPath(List(startState))
+    stateQueue.enqueue(startState)
+    return findPath(stateQueue)
     
 
   def runStep1(p: os.Path, f: String, debug:Boolean) : Long =
     val data : geny.Generator[String] = os.read.lines.stream(p / f)
-    val hm : HeatMap = HeatMap(data.toList)
-    val res = Utils.getTime(hm,debug,findPath(hm,1,3))
+    val hm : HeatMap = HeatMap(loadHeatMap(data.toList))
+    val res = Utils.getTime(debug,findPath(hm,1,3))
     return res
 
   def runStep2(p: os.Path, f: String, debug:Boolean) : Long = 
     val data : geny.Generator[String] = os.read.lines.stream(p / f)
-    val hm : HeatMap = HeatMap(data.toList)
-    val res = Utils.getTime(hm,debug,findPath(hm,4,10))
+    val hm : HeatMap = HeatMap(loadHeatMap(data.toList))
+    val res = Utils.getTime(debug,findPath(hm,4,10))
     return res
 
 
 @main def runDay17() = 
   val day = Day17()
-  // println("Step1 : Sample")
-  // println(day.runStep1(Utils.dataSamplePath,day.dataFileS1,true))
-  // println("Step1 : Full")
-  // println(day.runStep1(Utils.dataFullPath,day.dataFileFull,false))
-  // println("Step2 : Sample")
-  // println(day.runStep2(Utils.dataSamplePath,day.dataFileS1,true))
+  println("Step1 : Sample")
+  println(day.runStep1(Utils.dataSamplePath,day.dataFileS1,true))
+  println("Step1 : Full")
+  println(day.runStep1(Utils.dataFullPath,day.dataFileFull,false))
+  println("Step2 : Sample")
+  println(day.runStep2(Utils.dataSamplePath,day.dataFileS1,true))
   println("Step2 : Full")
   println(day.runStep2(Utils.dataFullPath,day.dataFileFull,false))
   
--- src/main/scala/tools/utils.scala
+++ src/main/scala/tools/utils.scala
@@ -12,3 +12,11 @@ object Utils :
     if debug then println("Elapsed Time : %s ms - Result : %s - Input : %s".format((a-b)/1000000,result,input))
     return result
   }
+
+  def getTime[T,A](debug:Boolean,fun : => T) : T =  {
+    val b = System.nanoTime()
+    val result = fun
+    val a = System.nanoTime()
+    if debug then println("Elapsed Time : %s ms - Result : %s".format((a-b)/1000000,result))
+    return result
+  }
